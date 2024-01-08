f453da81965ed0e7335e40786ba1bacd11520ae7
Muhammad-decode <153366449+Muhammad-decode@users.noreply.github.com>
2024-01-08 10:23:26 +0500

    Initial Commit

---
+++ Repairing App/Repairing_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Repairing_AppApp.swift
+//  Repairing App
+//
+//  Created by Muhammad Afnan on 08/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Repairing_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
